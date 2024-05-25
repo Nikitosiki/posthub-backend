a601511c68a90563c611d1b033bb6f124379e4e5
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-05-25 23:54:00 +0300

    update: routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use App\Http\Controllers\Api\v1\PostController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-db-connection', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'Database connection is successful';
+    } catch (\Exception $e) {
+        return 'Database connection failed: ' . $e->getMessage();
+    }
+});
+
+// Route::get(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\v1'], function () {
+//     Route::apiResource('/post', PostController::class);
+// });
+
+Route::prefix('api/v1')->group(function () {
+    Route::get('/posts', [PostController::class, 'posts']);
 });
